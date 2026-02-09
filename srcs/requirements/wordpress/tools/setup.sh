3e4274efe2a6dbd9fd2159151c1f08a1c89ce9c6
Incheol Jo <injo@c1r1s5.42gyeongsan.kr>
2026-02-09 16:06:15 +0900

    [add] wordpress conf, setup.sh

---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+sleep 10
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+    echo "wordpress downloading"
+    wp core download --allow-root
+    
+    wp config create --allow-root \
+        --dbname=${MYSQL_DATABASE} \
+        --dbuser=${MYSQL_USER} \
+        --dbpass=${MYSQL_PASSWORD} \
+        --dbhost=mariadb:3306
+
+    wp core install --allow-root \
+        --url=${DOMAIN_NAME} \
+        --title="Inception" \
+        --admin_user=${WP_ADMIN_USER} \
+        --admin_password=${WP_ADMIN_PASSWORD} \
+        --admin_email=${WP_ADMIN_EMAIL}
+
+    wp user create ${WP_USER_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author --allow-root
+
+    echo "wordpress installation completed!"
+else
+    echo "wordpress is already installed."
+fi
+
+exec "$@"
