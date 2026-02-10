ef24806f6e8e2abcfe1fce2e3cd628cc8395c282
Incheol Jo <injo@c1r1s5.42gyeongsan.kr>
2026-02-10 12:47:57 +0900

    [modify] mariadb init.sh & docker-compose depends on

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -15,10 +15,11 @@ echo "MariaDB is ready."
 
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
-    mysql -u root -p"${MYSQL_ROOT_PASSWORD}" << EOF
+    mysql -u root << EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
     echo "Database create"
